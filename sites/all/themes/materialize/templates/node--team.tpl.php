ec29b4645c2aa1dd5722ab36a155040f0bd07aee
Andrew Brown <ABrown@genuineinteractive.com>
2016-09-12 17:06:04 -0400

    Adding Team Record fields, team page updates.

--- sites/all/themes/materialize/templates/node--team.tpl.php
+++ sites/all/themes/materialize/templates/node--team.tpl.php
@@ -24,18 +24,31 @@
       hide($content['links']);
       hide($content['field_tags']);
     ?>
-    <div>
-      <strong>League: </strong><?php print render($content['og_group_ref'][0]); ?><br />
-      <strong>Owner: </strong><?php print render($content['field_team_owner'][0]); ?><br />
-      <strong>Current Record: </strong><?php print render($content['field_team_wins'][0]); ?> -
-              <?php print render($content['field_team_losses'][0]); ?> -
-              <?php print render($content['field_team_ties'][0]); ?><br />
+    <div class="row">
+      <div class="col s12 m12 l4">
+        <strong>League: </strong><?php print render($content['og_group_ref'][0]); ?><br />
+        <strong>Owner: </strong><?php print render($content['field_team_owner'][0]); ?><br />
+        <strong>Current Record: </strong><?php print render($content['field_team_wins'][0]); ?> -
+                <?php print render($content['field_team_losses'][0]); ?> -
+                <?php print render($content['field_team_ties'][0]); ?><br />
+      </div>
+      <div class="col s12 m12 l8">
+        <table class="striped">
+            <tbody>
+              <tr><td><strong>Highest Score </strong></td><td><?php print render($content['field_team_most_points'][0]); ?></td></tr>
+              <tr><td><strong>Lowest Score </strong></td><td><?php print render($content['field_team_least_points'][0]); ?></td></tr>
+              <tr><td><strong>Biggest Win </strong></td><td>+<?php print render($content['field_team_biggest_win'][0]); ?></td></tr>
+              <tr><td><strong>Biggest Loss </strong></td><td>-<?php print render($content['field_team_biggest_loss'][0]); ?></td></tr>
+            </tbody>
+          </table>
+        </div>
     </div>
       <?php
 //       print render($content);
       ?>
+    <div class="row">
     <h3>Historical Statistics</h3>
-    <table>
+    <table class="striped">
       <thead>
       <tr>
         <th>Year</th>
@@ -54,7 +67,6 @@
 
       foreach($content['field_historical_stats']['#items'] as $history) {
         $history_field = entity_load('field_collection_item', $history);
-
         foreach($history_field as $history_object) {
           $total['wins'] += $history_object->field_historical_wins['und'][0]['value'];
           $total['losses'] += $history_object->field_historical_losses['und'][0]['value'];
@@ -75,17 +87,18 @@
         }
       }
       print '<tr class="historical_totals">';
-      print '<td colspan="3">Totals</td>';
-      print '<td>'.$total['wins'].'</td>';
-      print '<td>'.$total['losses'].'</td>';
-      print '<td>'.$total['ties'].'</td>';
-      print '<td>'.$total['pf'].'</td>';
-      print '<td>'.$total['pa'].'</td>';
+      print '<td colspan="3"><strong>Totals</strong></td>';
+      print '<td><strong>'.$total['wins'].'</strong></td>';
+      print '<td><strong>'.$total['losses'].'</strong></td>';
+      print '<td><strong>'.$total['ties'].'</strong></td>';
+      print '<td><strong>'.$total['pf'].'</strong></td>';
+      print '<td><strong>'.$total['pa'].'</strong></td>';
       print '</tr>';
 
       ?>
       </tbody>
     </table>
+    </div>
   </div>
     
     <?php if (($tags = render($content['field_tags'])) || ($links = render($content['links']))): ?>
